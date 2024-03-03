242f80f758a8e11d30237b036e8e8566bbe7dee2
cyber99king <hafizur4r@gmail.com>
2024-03-04 02:14:02 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,9 +28,22 @@ Route::get('/student-list', [App\Http\Controllers\StudentController::class, 'stu
 Route::get('/student/{id}', [App\Http\Controllers\StudentController::class, 'student_edit'])->name('student.edit');
 Route::put('/student/{id}', [App\Http\Controllers\StudentController::class, 'student_update'])->name('student.update');
 Route::delete('/student/{id}', [App\Http\Controllers\StudentController::class, 'student_delete'])->name('student.delete');
-Route::get('/education', [App\Http\Controllers\EducationControlller::class, 'education'])->name('education');
-Route::post('/education', [App\Http\Controllers\EducationControlller::class, 'store_education'])->name('store.education');
-Route::get('/education', [App\Http\Controllers\EducationControlller::class, 'education_list'])->name('education.list');
-Route::get('/education/{id}', [App\Http\Controllers\EducationControlller::class, 'education_edit'])->name('education.edit');
-Route::put('/education/{id}', [App\Http\Controllers\EducationControlller::class, 'education_update'])->name('education.update');
-Route::delete('/education/{id}', [App\Http\Controllers\EducationControlller::class, 'education_delete'])->name('education.delete');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/education', [App\Http\Controllers\EducationControlller::class, 'education'])->name('education');
+// Route::post('/education', [App\Http\Controllers\EducationControlller::class, 'store_education'])->name('store.education');
+// Route::get('/education', [App\Http\Controllers\EducationControlller::class, 'education_list'])->name('education.list');
+// Route::get('/education/{id}', [App\Http\Controllers\EducationControlller::class, 'education_edit'])->name('education.edit');
+// Route::put('/education/{id}', [App\Http\Controllers\EducationControlller::class, 'education_update'])->name('education.update');
+// Route::delete('/education/{id}', [App\Http\Controllers\EducationControlller::class, 'education_delete'])->name('education.delete');
